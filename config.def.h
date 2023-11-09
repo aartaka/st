dcb8d51a857c925e634506e524cf37f4d05e52cc
Artyom Bologov <aartaka@protonmail.com>
2023-11-10 01:30:52 +0400

    config.gef.h: My font and color setup.

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Hack:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -96,32 +96,32 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#090000",
+	"#cd5c5c",
+	"#556b2f",
+	"#ffa408",
+	"#3457d5",
+	"#6f00ff",
+	"#87d3f8",
+	"#ffe3e3",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#250000",
+	"#fa8072",
+	"#6b8e23",
+	"#ffb82b",
+	"#1f75fe",
+	"#8a2be2",
+	"#afdbf5",
+	"#ffffff",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+        "#cccccc",
+	"#ffffbb",
+	"#ffe3e3", /* default foreground colour */
+	"#090000", /* default background colour */
 };
 
 
